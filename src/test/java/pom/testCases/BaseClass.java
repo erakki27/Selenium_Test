af8ccf110c277fe6c446aff56912b2dd6d171c69
erakki27 <erakki27@gmail.com>
2024-06-27 19:07:55 +0530

    final fix

--- src/test/java/pom/testCases/BaseClass.java
+++ src/test/java/pom/testCases/BaseClass.java
@@ -1,10 +1,12 @@
 package pom.testCases;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 
 import org.testng.annotations.BeforeClass;
@@ -15,6 +17,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
@@ -31,21 +35,14 @@ public class BaseClass {
 	public static WebDriver driver;
 	public static Logger Logger;
 	
-	@SuppressWarnings("static-access")
 	@BeforeClass
 	public void setup() {
 		//System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
-		 ChromeOptions chromeOptions = new ChromeOptions();
-		 chromeOptions.setCapability("platformName", "Windows 10");
 		 
-		 chromeOptions.addArguments("--remote-allow-origins=*");
-		 
-		 WebDriverManager.chromedriver().setup();
-		    driver = new ChromeDriver();
 		    
-		
-		Logger = Logger.getLogger("Demo");
-		PropertyConfigurator.configure("log4j.properties");
+				driver = new ChromeDriver();
+				Logger = Logger.getLogger("Demo");
+				PropertyConfigurator.configure("log4j.properties");
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(BaseUrl);
